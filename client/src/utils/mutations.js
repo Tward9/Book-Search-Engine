79d0fd65057d3cca077af997e2072a205fea3513
Theo Ward <theoward9@gmail.com>
2022-06-27 17:49:05 -0500

    added front end mutations save and remove book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,5 +26,41 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-    mutation 
-`
+    mutation SaveBook($username: String!, $authors: [String]!, $description:    String!, $bookId: String!, $image: String!, $link: String!, $title: String!) {
+        saveBook(username: $username, authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+            _id
+            username
+            email
+            password
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation RemoveBook($username: String!, $bookId: String!) {
+        removeBook(username: $username, bookId: $bookId) {
+            _id
+            username
+            email
+            password
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
