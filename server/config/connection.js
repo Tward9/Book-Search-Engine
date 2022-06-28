e8936dcffb409b5e656b0340e222eb61d4dc9b52
Theo Ward <theoward9@gmail.com>
2022-06-28 18:49:54 -0500

    removed exposed Link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://theoward9:Codinfms13!@cluster0.qc3fv.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
